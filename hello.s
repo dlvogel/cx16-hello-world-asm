0d6f7fb3a545e5f87ba5a5d0185d8d3975b630d2
dlvogel <dlvogel@gmail.com>
2020-08-01 20:59:10 -0700

    Cleaned up and commented
    Updated the README file.  Commented code for posterity.

--- hello.s
+++ hello.s
@@ -1,14 +1,21 @@
-CHAROUT = $FFD2
+.org $0801
 
+CHAROUT = $FFD2 ; Address of CHAROUT Kernal Routine
+
+;The following line creates the basic statement "1 SYS 2061"  
+;so that when the RUN command is issued it will jump to the 
+;program(which starts at location 2061 or $81D) and run it. 
 .byte $0b,$08,$01,$00,$9e,$32,$30,$36,$31,$00,$00,$00 
 
-        LDX #0
+        LDX #$00        
+        LDA msg         ;Load first character in the Accumulator
 
-loop:   LDA msg,X
-        JSR CHAROUT
+ loop:  JSR CHAROUT     ;Print character in the Accumulator to the output device(default is the screen)
         INX
-        CPX #$0c
-        BNE loop
-        RTS
+        LDA msg,X       ;Load next character in the Accumulator
+        CMP #$00        ;Check Accumulator for $00, which indicates end of the message
+        BNE loop        ;If the next character  is not $00 go to the top of the loop
 
-    msg: .byte "HELLO WORLD!"
+        RTS             ;Exit routine and return to BASIC
+
+    msg: .byte "hello world!",$00
