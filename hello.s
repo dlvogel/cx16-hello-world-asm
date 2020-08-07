e5b882f80b35efd6f2f8e6c2b003c8ed6ccbdb8e
dlvogel <dlvogel@users.noreply.github.com>
2020-08-06 20:02:47 -0700

    Corrected typo in comments
    Comments incorrectly stated the program starts at $81D, not at the correct address of $80D.

--- hello.s
+++ hello.s
@@ -4,7 +4,7 @@ CHAROUT = $FFD2 ; Address of CHAROUT Kernal Routine
 
 ;The following line creates the basic statement "1 SYS 2061"  
 ;so that when the RUN command is issued it will jump to the 
-;program(which starts at location 2061 or $81D) and run it. 
+;program(which starts at location 2061 or $80D) and run it. 
 .byte $0b,$08,$01,$00,$9e,$32,$30,$36,$31,$00,$00,$00 
 
         LDX #$00        
